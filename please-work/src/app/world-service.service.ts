84e13f9ae303eb9f4d5b41a06686a658b8655b56
Cano <canojav@amazon.com>
2024-08-22 14:03:07 -0700

    add

--- please-work/src/app/world-service.service.ts
+++ please-work/src/app/world-service.service.ts
@@ -8,9 +8,28 @@ export class WorldServiceService {
 
   constructor(private http: HttpClient) { 
   }
-  apiURL = 'http://api.worldbank.org/v2/country/'
-  getCountryInfo(Name: String){
-    const info = this.http.get(this.apiURL + Name +'?format=json')
+  apiURL = 'https://api.worldbank.org/v2/country/'
+  getCountryInfo(cName: String){
+    const info = this.http.get(this.apiURL + cName +'?format=json')
     return info
+  }
+}
+export class dataCountry{
+  countryName!: String;
+  capital!: String;
+  region!: String;
+  incomeLevel!: String;
+  longitude!: String;
+  latitude!: String;
+  
+  showInfo(){
+    [
+      {'Country': this.countryName},
+      {'Capital': this.capital},
+      {'Region': this.region},
+      {'Income Level': this.incomeLevel},
+      {'Longitude': this.longitude},
+      {'Latidude': this.latitude}
+    ]
   }
 }
--- please-work/src/app/world/world.component.ts
+++ please-work/src/app/world/world.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
+import { dataCountry, WorldServiceService } from '../world-service.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-world',
@@ -7,6 +9,36 @@ import { Component } from '@angular/core';
   templateUrl: './world.component.html',
   styleUrl: './world.component.css'
 })
-export class WorldComponent {
+export class WorldComponent implements OnInit{
+  constructor(public myAPI: WorldServiceService){}
+  @Input() container?: dataCountry
+  @Output() targetCountry = new EventEmitter<any>()
 
+  countryInfo = new dataCountry
+  dataInfo: any
+
+  ngOnInit(): void {
+    const targetMap = document.querySelector('svg')
+    const targetPaths = targetMap?.querySelectorAll('path')
+    targetPaths?.forEach((path: SVGPathElement) => {
+      path.addEventListener('click', this.onClick.bind(this))
+    });
+  }
+  onClick(event: MouseEvent){
+    const paths = event.target as SVGAElement
+    const clickID = paths.id
+    this.dataInfo = this.myAPI.getCountryInfo(clickID).subscribe((data: any) => {
+    this.countryInfo.countryName = data[1][0].name
+    this.countryInfo.capital = data[1][0].capitalCity
+    this.countryInfo.region = data[1][0].region.value
+    this.countryInfo.incomeLevel = data[1][0].incomeLevel.value
+    this.countryInfo.longitude= data[1][0].longitude
+    this.countryInfo.latitude = data[1][0].latitude
+    console.log(
+      data[1],
+      this.countryInfo
+    )
+    this.targetCountry.emit(this.countryInfo)
+    })
+  }
 }
